89b950fa7b26f652c51323f85a2b9c1bde3a7c5f
Naveen-T <naveentm95@gmail.com>
2021-08-11 19:46:34 +0530

    Update bank-loan.view-spec.ts

--- bank-loan.view-spec.ts
+++ bank-loan.view-spec.ts
@@ -1,44 +1,43 @@
 import { HomeLoanPO } from "../../../home-loan.po";
-import { GoToPage } from "../../../generic-helpers";
-
-let homeLoan : HomeLoanPO;
 
 describe('ANZ Home Loan', () => {
   beforeAll(async () => {
-      await GoToPage.bySuburl('https://www.anz.com.au/personal/home-loans/calculators-tools/much-borrow/');
+      WebDriver driver = new FirefoxDriver();
+      driver.manage().window().maximise();
+      driver.get('https://www.anz.com.au/personal/home-loans/calculators-tools/much-borrow/');
   });
   
   describe('Home Loan', () => {
     beforeAll(async () => {
-        await homeLoan.applicationType.click();
-        await homeLoan.dependancts.click();
-        await homeLoan.propertyType.click();
-        await homeLoan.income.click();
-        await homeLoan.income.sendkeys('80000');
-        await homeLoan.outherIncome.click();
-        await homeLoan.outherIncome.sendkeys('10000');
-        await homeLoan.livingExpense.click();
-        await homeLoan.livingExpense.sendkeys('500');
-        await homeLoan.otherLoan.click();
-        await homeLoan.otherLoan.sendkeys('100');
-        await homeLoan.creditCardLimit.click();
-        await homeLoan.creditCardLimit.sendkeys('10000');
+        await driver.applicationType.click();
+        await driver.dependancts.click();
+        await driver.propertyType.click();
+        await driver.income.click();
+        await driver.income.sendkeys('80000');
+        await driver.outherIncome.click();
+        await driver.outherIncome.sendkeys('10000');
+        await driver.livingExpense.click();
+        await driver.livingExpense.sendkeys('500');
+        await driver.otherLoan.click();
+        await driver.otherLoan.sendkeys('100');
+        await driver.creditCardLimit.click();
+        await driver.creditCardLimit.sendkeys('10000');
     });
   
     it('Validate borrow estimation for a person', async () => {
-        await homeLoan.workOutButton.click();
-        expect(await homeLoan.result.getText()).toEqual('$479,000');
+        await driver.workOutButton.click();
+        expect(await driver.result.getText()).toEqual('479,000');
     });
     
     it('Validate start over button to clear the form', async () => {
-        await homeLoan.startOver.click();
-        expect(await homeLoan.workOutButton.isDisplayed()).toEqual(true);
+        await driver.startOver.click();
+        expect(await driver.income.getText()).toEqual('0');
     });
 
     it('Validate error message when uesr enter the living expense as $1', async () => {
-        await homeLoan.livingExpense.click();
-        await homeLoan.livingExpense.sendkeys('1');
-        expect(await homeLoan.result.getText()).toEqual("Based on the details you've entered, we're unable to give you an estimate of your borrowing power with this calculator. For questions, call us on 1800 100 641.");
+        await driver.livingExpense.click();
+        await driver.livingExpense.sendkeys('1');
+        expect(await driver.result.getText()).toEqual("Based on the details you've entered, we're unable to give you an estimate of your borrowing power with this calculator. For questions, call us on 1800 100 641.");
     });
   });
 });
