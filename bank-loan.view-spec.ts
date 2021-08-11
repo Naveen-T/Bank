565cb88ff27e4482abf31233734ac39b7feb975d
Naveen-T <naveentm95@gmail.com>
2021-08-11 14:25:24 +0530

    Update bank-loan.view-spec.ts

--- bank-loan.view-spec.ts
+++ bank-loan.view-spec.ts
@@ -5,37 +5,40 @@ let homeLoan : HomeLoanPO;
 
 describe('ANZ Home Loan', () => {
   beforeAll(async () => {
-      await GoTo
-  });
-  beforeAll(async () => {
-      await homeLoan.applicationType.click();
-      await homeLoan.dependancts.click();
-      await homeLoan.propertyType.click();
-      await homeLoan.income.click();
-      await homeLoan.income.sendkeys('80000');
-      await homeLoan.outherIncome.click();
-      await homeLoan.outherIncome.sendkeys('10000');
-      await homeLoan.livingExpense.click();
-      await homeLoan.livingExpense.sendkeys('500');
-      await homeLoan.otherLoan.click();
-      await homeLoan.otherLoan.sendkeys('100');
-      await homeLoan.creditCardLimit.click();
-      await homeLoan.creditCardLimit.sendkeys('10000');
+      await GoToPage.navigateTo('hhtps://www.anz.com.au/personal/home-loans/calculators-tools/much-borrow/');
   });
   
-  it('Validate borrow estimation for a person', async () => {
-      await homeLoan.workOutButton.click();
-      expect(await homeLoan.result.getText()).toEqual('$479,000');
-  });
+  describe('Home Loan', () => {
+    beforeAll(async () => {
+        await homeLoan.applicationType.click();
+        await homeLoan.dependancts.click();
+        await homeLoan.propertyType.click();
+        await homeLoan.income.click();
+        await homeLoan.income.sendkeys('80000');
+        await homeLoan.outherIncome.click();
+        await homeLoan.outherIncome.sendkeys('10000');
+        await homeLoan.livingExpense.click();
+        await homeLoan.livingExpense.sendkeys('500');
+        await homeLoan.otherLoan.click();
+        await homeLoan.otherLoan.sendkeys('100');
+        await homeLoan.creditCardLimit.click();
+        await homeLoan.creditCardLimit.sendkeys('10000');
+    });
   
-  it('Validate start over button to clear the form', async () => {
-      await homeLoan.startOver.click();
-      expect(await homeLoan.workOutButton.isDisplayed()).toEqual(true);
-  });
-  
-  it('Validate error message when uesr enter the living expense as $1', async () => {
-      await homeLoan.livingExpense.click();
-      await homeLoan.livingExpense.sendkeys('1');
-      expect(await homeLoan.result.getText()).toEqual("Based on the details you've entered, we're unable to give you an estimate of your borrowing power with this calculator. For questions, call us on 1800 100 641.");
+    it('Validate borrow estimation for a person', async () => {
+        await homeLoan.workOutButton.click();
+        expect(await homeLoan.result.getText()).toEqual('$479,000');
+    });
+
+    it('Validate start over button to clear the form', async () => {
+        await homeLoan.startOver.click();
+        expect(await homeLoan.workOutButton.isDisplayed()).toEqual(true);
+    });
+
+    it('Validate error message when uesr enter the living expense as $1', async () => {
+        await homeLoan.livingExpense.click();
+        await homeLoan.livingExpense.sendkeys('1');
+        expect(await homeLoan.result.getText()).toEqual("Based on the details you've entered, we're unable to give you an estimate of your borrowing power with this calculator. For questions, call us on 1800 100 641.");
+    });
   });
 });
